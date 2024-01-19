03784a0b1347f0cb4211d553e803bb3f3e131ec0
stefanopaesano <stefanopaesano022@gmail.com>
2024-01-19 17:49:06 +0100

    modifiche array

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,86 +1,88 @@
 <script>
+import Singleproduct from './singleproduct.vue';
+
 export default {
     data() {
         return {
-            images:[
-  {
-      "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
-      "price": "$19.99",
-      "series": "Action Comics",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
-      "price": "$3.99",
-      "series": "American Vampire 1976",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
-      "price": "$16.99",
-      "series": "Aquaman",
-      "type": "graphic novel",
-  },
-  {
-      "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
-      "price": "$2.99",
-      "series": "Batgirl",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
-      "price": "$3.99",
-      "series": "Batman",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
-      "price": "$2.99",
-      "series": "Batman Beyond",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
-      "price": "$3.99",
-      "series": "Batman/Superman",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
-      "price": "$4.99",
-      "series": "Batman/Superman Annual",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
-      "price": "$5.99",
-      "series": "Batman: The Joker War Zone",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
-      "price": "$6.99",
-      "series": "Batman: Three Jokers",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
-      "price": "$4.99",
-      "series": "Batman: White Knight Presents: Harley Quinn",
-      "type": "comic book",
-  },
-  {
-      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
-      "price": "$16.99",
-      "series": "Catwoman",
-      "type": "graphic novel",
-  },
-]
-
+            images: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560,c_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel",
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel",
+                },
+            ]
         };
     },
-    methods: {
-
+    methods: {},
+    components: {
+         Singleproduct 
     }
 }
 </script>
@@ -99,11 +101,17 @@ export default {
                     </div>
 
                     <div class="container-prodotti">
+                        <Singleproduct  v-for="(image,i) in images" :key="i"/>
 
-                        <div class="unico-prodotto" v-for="(image,i) in images" :key="i">
-                            {{ image.thumb }}
+                        <!-- <div class="unico-prodotto" v-for="(image,i) in images" :key="i">
 
-                        </div>
+                            <div class="tutte-img">
+                                <img :src="image.thumb" :alt="image.price">
+                            </div>
+                            
+                            {{ image.series }}
+
+                        </div> -->
 
                     </div>
 
@@ -268,14 +276,32 @@ export default {
 .bar-due{
     border: 1px solid black;
     width: 100%;
-    height: 150px;
+    min-height: 150px;
     background-color: black;
     color: white;
-        .container-prodotti{
-            display: flex;
-            flex-wrap: wrap;
-                    .sin
-        }
+        // .container-prodotti{
+        //     display: flex;
+        //     flex-wrap: wrap;
+//                     .unico-prodotto{
+//                         width: 25%;
+//                         padding: 40px;
+//                         border: 1px solid white;
+//                         text-align: center;
+
+//                         .tutte-img{
+//                             width: 100%;
+//                                 img {
+//                                     width: 100%;
+//                                     max-width: 100%;
+//                                     height: auto; 
+//                                 }
+
+//                         }
+
+//                     }
+
+//         }
+
 }
 
 
---
+++ src/components/singleproduct.vue
@@ -0,0 +1,46 @@
+<script>
+export default{
+      data(){
+
+        return{
+            product:{
+                
+            }
+
+        };
+      }
+}
+</script>
+
+<template>
+    <div class="unico-prodotto" v-for="(image,i) in images" :key="i">
+
+        <div class="tutte-img">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg" alt="19,97">
+        </div>
+
+        {{ image.series }}
+
+    </div>
+</template>
+
+<style lang="scss"scoped>
+.unico-prodotto{
+         width: 25%;
+        padding: 40px;
+        border: 1px solid white;
+        text-align: center;
+
+        .tutte-img{
+         width: 100%;
+         img {
+            width: 100%;
+             max-width: 100%;
+            height: auto; 
+            }
+
+        }
+ }
+        
+
+</style>
